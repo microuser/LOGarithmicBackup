e4311704f20081306a515b071c014515e1492036
Paul Richeson <microuser@perje.net>
2023-11-30 17:43:39 -0800

    Update LOGarithmicBackup.sh
    There once was an upBoat

--- LOGarithmicBackup.sh
+++ LOGarithmicBackup.sh
@@ -20,5 +20,5 @@ tar -zcvf /home/dmp/DMPServer/$archiveBase$thisYear-$thisMonth-$thisDay-$thisHou
 
 cd /home/dmp/DMPServer
 find . -name "$archiveBase*.tar.gz" -regextype sed -not -regex "\(.*/$archiveBase$thisYear-$thisMonth-$thisDay-$thisHour-$thisTenMinute[0-9]-[0-9][0-9].tar.gz\)\|\(.*/$archiveBase$thisYear-$thisMonth-$thisDay-$thisHour-[0-9]0-[0-9][0-9].tar.gz\)\|\(.*/$archiveBase$thisYear-$thisMonth-$thisDay-[0-2][0-9]-00-[0-9][0-9].tar.gz\)\|\(.*/$archiveBase$thisYear-$thisMonth-[0-9][0-9]-00-00-[0-9][0-9].tar.gz\)\|\(.*/$archiveBase$thisYear-[0-1][0-9]-01-00-00-[0-9][0-9].tar.gz\)\|\(.*/$archiveBase20[0-9][0-9]-01-01-00-00-[0-9][0-9].tar.gz\)" -exec echo {} \;
-
+#bump that amazing sed one-liner filtering for the records
 
